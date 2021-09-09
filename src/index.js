179ad461f80294b9bc86f016c79325db2d6519dc
krishnaraj1988 <90171996+krishnaraj1988@users.noreply.github.com>
2021-09-09 15:02:14 +0530

    Created new branch

--- src/index.js
+++ src/index.js
@@ -5,6 +5,5 @@ import render from './render';
 import './registerEventListeners';
 
 const rootElement = document.getElementById('demo');
-
 store.subscribe(state => render(rootElement, state));
 render(rootElement, store.getState());
